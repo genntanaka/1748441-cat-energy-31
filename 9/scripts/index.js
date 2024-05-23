f7ec1f43cf1d58d263bec1aad45181aeb0d2c15f
genntanaka <genntanaka@mail.ru>
2024-05-23 03:53:09 +0000

    Deploying to gh-pages from @ genntanaka/1748441-cat-energy-31@09702faf1b07c1f759a0b677cbc0661c773b3344 🚀

--- 9/scripts/index.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("toggle").onclick=e;function e(){this.parentNode.classList.toggle("main-nav--open"),document.body.classList.toggle("page__body--open")}
---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+document.getElementById("toggle").onclick=e;function e(){this.parentNode.classList.toggle("main-nav--open"),document.body.classList.toggle("page__body--open")}
